4d06ff7bb7593b57a3297bf0b8008de6badf613a
Aatif <aatifkhan2001@gmail.com>
2024-10-19 16:38:15 +0530

    added a file

---
+++ 1ClassAndObject.cpp
@@ -0,0 +1,15 @@
+
+//1 program to create class and object
+#include<iostream>
+using namespace std;
+  class HERO{
+    //properties
+    int health;
+  };
+  
+int main(){
+  //creation of object's
+  HERO h1;
+  cout<<"size :" <<sizeof(h1)<<endl;
+  return 0;
+}
